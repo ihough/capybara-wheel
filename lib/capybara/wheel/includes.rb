5a4f7aad3529507e70e56aaa761494dcb36d4b3b
Ian Hough <ijhough@gmail.com>
2014-12-19 16:21:08 -0500

    Better parsing of element names - deal with whitespace - camelize rather than capitalize

--- lib/capybara/wheel/includes.rb
+++ lib/capybara/wheel/includes.rb
@@ -6,17 +6,23 @@ module Capybara
       module ClassIncludes
         # Allow defining an element on a page or within another element
         def element(name, _selector, &block)
-          # Set up the element as CurrentClass::ElementName
+          # Create a class inheriting from Capybara::Wheel::Element
           klass = Class.new Capybara::Wheel::Element
-          begin
-            const_set name, klass
-          rescue NameError
-            puts "Cannot create class '#{name}' -- trying '#{name.capitalize}'"
-            name.capitalize!
-            retry
-          end
 
-          # Define ElementName.selector and ElementName#selector
+          # Name the class CurrentClass::ElementName
+          if name.match /\s/
+            new_name = name.split(/\s+/).map(&:capitalize).join
+            warn "Element name '#{name}' contains whitespace -- changing to '#{new_name}'"
+            name = new_name
+          end
+          unless name.chars.first == name.chars.first.capitalize
+            new_name = name.split(/([A-Z]+[a-z]*)/).map(&:capitalize).join
+            warn "Element name '#{name}' is not camelcased -- changing to '#{new_name}'"
+            name = new_name
+          end
+          const_set name, klass
+
+          # Define .selector and #selector on CurrentClass::ElementName
           klass.singleton_class.class_eval do
             define_method(:selector) { @selector = _selector }
           end
@@ -24,21 +30,25 @@ module Capybara
             def_delegator self, :selector
           end
 
-          # Define CurrentClass#element_name to return an ElementName instance
-          # whose selector will be scoped to within CurrentClass' selector
+          # Define CurrentClass#element_name to return an instance of
+          # CurrentClass::ElementName whose scope is set to the instance of
+          # CurrentClass -- this allows scoping capybara methods
           define_method(underscore(name).to_sym) { klass.new self }
 
-          # Define any subelements and methods on ElementName
+          # Evaluate any block in the context of CurrentClass::ElementName
           klass.class_eval(&block) if block
+
+          # Return CurrentClass::ElementName
+          klass
         end
 
         # For use by .element -- from rails String#underscore
         def underscore(string)
           string.gsub(/::/, '/').
-                  gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-                  gsub(/([a-z\d])([A-Z])/,'\1_\2').
-                  tr("-", "_").
-                  downcase
+                 gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+                 gsub(/([a-z\d])([A-Z])/,'\1_\2').
+                 tr("-", "_").
+                 downcase
         end
       end
     end
