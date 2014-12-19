7e312a2af566752a9e86e3be37cf49d58b12b08a
Ian Hough <ijhough@gmail.com>
2014-12-19 16:26:58 -0500

    Page tests

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -1,23 +1,19 @@
 require 'spec_helper'
 
 describe Capybara::Wheel::Page do
-  let(:klass) { subject.class }
-
-  describe '.element' do
-    it 'creates a subclass for the element and sets up an instance method' do
-      expect { klass::RadElement }.to raise_error(NameError, /uninitialized constant/ )
-      expect(subject).not_to respond_to(:rad_element)
-
-      klass.element('RadElement', '#rad_element')
-
-      expect { klass::RadElement }.not_to raise_error
-      expect(subject.rad_element).to be_a(klass::RadElement)
-    end
+  it 'extends Capybara::Wheel::Includes::ClassIncludes' do
+    expect(described_class).to respond_to(:element)
   end
 
   describe '#capybara' do
     it 'provides access to the capybara instance' do
       expect(subject.capybara).to be_a Capybara::Session
+    end
+  end
+
+  describe '#capybara_element' do
+    it 'is an alias for #capybara' do
+      expect(subject.capybara_element).to eq(subject.capybara)
     end
   end
 
@@ -34,8 +30,10 @@ describe Capybara::Wheel::Page do
   end
 
   describe '#on' do
+    pending 'TODO'
   end
 
   describe 'visit' do
+    pending 'TODO'
   end
 end
